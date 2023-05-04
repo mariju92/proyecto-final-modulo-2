4d48b128e77ccf9447f4eede4cbb99ee16a203f1
Joaquin Fuentes <joaquin.fuentes1327@gmail.com>
2023-05-04 19:19:20 -0300

    crear usuario y guardar en localstorage funcionando

--- js/administradorUsuario.js
+++ js/administradorUsuario.js
@@ -6,14 +6,57 @@ const contrasenia = document.querySelector("#contrasenia");
 const confirmarContrasenia = document.querySelector("#confirmarContrasenia");
 const formCargarUsuario = document.querySelector("#formCargarUsuario")
 
+let listaUsuarios = localStorage.getItem("listaUsuarios");
+if (!listaUsuarios){
+    listaUsuarios = []
+} else{
+    listaUsuarios = JSON.parse(listaUsuarios).map((usuario)=>{
+        new Usuario(
+            usuario.id,
+            usuario.nombre,
+            usuario.email,
+            usuario.contrasenia,
+            usuario.rol
+        )
+    })
+}
 
 
-formCargarUsuario.addEventListener("submit" , cargarPelicula);
+formCargarUsuario.addEventListener("submit" , cargarUsuario);
 
 
 
 
-function cargarPelicula(e){
+function cargarUsuario(e){
     e.preventDefault()
-    console.log("aqui se carga la pelicula")
+    console.log("aqui se carga el usuario")
+    crearUsuario();
+
+}
+function crearUsuario(){
+    //validar datos
+    //crear el usuario
+    let nuevoUsuario = new Usuario(
+        undefined,
+        nombre.value,
+        email.value,
+        contrasenia.value,
+        undefined
+        )
+    
+        listaUsuarios.push(nuevoUsuario);
+        console.log(nuevoUsuario)
+    //guardar el usuario en localstorage
+    guardarUsuarioEnLocalStorage();
+    //limpiar el formulario
+    limpiarFormularioUsuario();
+    //dibujar la fila al final de la tabla
+    //mostrar cartel al usuario de que se creo corretamente
+}
+
+function guardarUsuarioEnLocalStorage(){
+    localStorage.setItem("listaUsuarios", JSON.stringify(listaUsuarios))
+}
+function limpiarFormularioUsuario(){
+    formCargarUsuario.reset()
 }
