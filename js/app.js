f0f16f1698d1dce59556455fb51ba770d8aa999d
Leandro Quipildor <83482016+LeooQuipildor@users.noreply.github.com>
2023-05-08 08:32:41 -0300

    Solucione un error que al buscar los productos me filtraba todas las cards de la pagina

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 document.addEventListener("keyup", function(e) {
     if (e.target.matches("#buscador")) {
-      let productos = document.querySelectorAll(".card");
+      let productos = document.querySelectorAll(".cardsProductos");
       let resultadosEncontrados = false;
   
       for (let i = 0; i < productos.length; i++) {
@@ -15,8 +15,9 @@ document.addEventListener("keyup", function(e) {
   
       let seccionProductos = document.getElementById("seccionProductos");
       if (!resultadosEncontrados) {
-        let mensaje = document.createElement("p");
+        let mensaje = document.createElement("h4");
         mensaje.textContent = "No se encontraron productos.";
+        mensaje.className = "text-center p-4"
         mensaje.id = "mensajeNoProducto";
         seccionProductos.innerHTML = "";
         seccionProductos.appendChild(mensaje);
