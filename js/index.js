effed5c03a9e10210a0d20169e900b540d9312fc
roxania <groxania@gmail.com>
2023-05-08 01:04:57 -0300

    termine la logica que muestra el detalle del producto

---
+++ js/index.js
@@ -0,0 +1,27 @@
+let listaProducto =
+    JSON.parse(localStorage.getItem("listaProducto")) || [];
+
+//dibujar columnas
+listaProducto.map((producto) => {
+    crearColumna(producto);
+});
+
+function crearColumna(producto) {
+    let grilla = document.querySelector("#grilla");
+    grilla.innerHTML += `
+    <aside class="col-12 col-md-4 col-lg-3 mb-3">
+    <div class="card h-100" >
+      <img src="${producto.imagen}" class="card-img-top" alt="${producto.nombre}">
+      <div class="card-body">
+        <h5 class="card-title">${producto.precio}</h5>
+      </div>
+      <div class='card-footer'>
+      <button class="btn btn-primary" onclick="detalleProducto('${producto.codigo}')">detalle</button>
+      </div>
+    </div>
+  </aside>
+    `;
+}
+window.detalleProducto = (codigo) => {
+    window.location.href = window.location.origin + '/pages/detalles.html?codigo=' + codigo
+}
