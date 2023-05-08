1a1d661d88ebe72ad1f6c14f12a1c8ad46749518
Leandro Quipildor <83482016+LeooQuipildor@users.noreply.github.com>
2023-05-08 11:58:16 -0300

    Solucione unos errores que daba al linkear un producto con la pagina de detalle

--- js/index.js
+++ js/index.js
@@ -1,20 +1,47 @@
-let listaProducto =
-    JSON.parse(localStorage.getItem("listaProducto")) || [];
+import Producto from "./claseProducto.js";
 
-//dibujar columnas
-listaProducto.map((producto) => {
-    crearColumna(producto);
-});
+let listaProducto = localStorage.getItem(`listaProducto`);
+if (!listaProducto) {
+  listaProducto = [];
+} else {
+  listaProducto = JSON.parse(listaProducto).map(
+    (producto) =>
+      new Producto(
+        producto.codigo,
+        producto.nombre,
+        producto.precio,
+        producto.categoria,
+        producto.imagen,
+        producto.descripcion,
+        producto.stock,
+        producto.destacado
+      )
+  );
+}
 
-function crearColumna(producto) {
+cargaInicial();
+
+function cargaInicial(){
+  //verificar si listaProductos tiene datos
+  if(listaProducto.length > 0){
+    //dibuja los datos en la tabla
+    listaProducto.map((producto, indice)=> crearCardProducto(producto, indice))
+  }
+}
+
+
+function crearCardProducto(producto) {
     let grilla = document.querySelector("#mostrarProductos");
     grilla.innerHTML += `
     <div class="col-9 col-md-4 col-lg-3 p-3 text-center cardsProductos">
-      <div class="card border-0 m-2"><a href="./pages/error404.html">
-        <img class="card-img-top border-1 bordeNaranja imgCard" src="${producto.imagen}" alt="${producto.nombre}"></a>
-        <div class="card-body fondoNegro"><a href="./pages/error404.html" class="text-decoration-none">
-          <h4 class="card-title text-white espacioTitulo">${producto.nombre}</h4>
-          <h5 class="colorNaranja">$${producto.precio}</h5>
+      <div class="card border-0 m-2">
+      <a href="./pages/detalles.html?codigo=${producto.codigo}">
+          <img class="card-img-top border-1 bordeNaranja imgCard" src="${producto.imagen}" alt="${producto.nombre}">
+        </a>
+        <div class="card-body fondoNegro">
+          <a href="./pages/error404.html" class="text-decoration-none">
+            <h4 class="card-title text-white espacioTitulo">${producto.nombre}</h4>
+            <h5 class="colorNaranja">$${producto.precio}</h5>
           </a>
           <form class="container">
             <div class="input-group py-2 pe-3 pe-md-0 d-flex justify-content-center ">
@@ -24,24 +51,9 @@ function crearColumna(producto) {
         </div>
       </div>
     </div>
-
-
-    
     `;
 }
 
-/*
-<aside class="col-12 col-md-4 col-lg-3 mb-3">
-    <div class="card h-100" >
-      <img src="${producto.imagen}" class="card-img-top" alt="${producto.nombre}">
-      <div class="card-body">
-        <h5 class="card-title">${producto.precio}</h5>
-      </div>
-      <div class='card-footer'>
-      <button class="btn btn-primary" onclick="detalleProducto('${producto.codigo}')">detalle</button>
-      </div>
-    </div>
-  </aside> */
 window.detalleProducto = (codigo) => {
-    window.location.href = window.location.origin + '/pages/detalles.html?codigo=' + codigo
+    window.location.href = window.location.origin + './pages/detalles.html?codigo=' + codigo
 }
