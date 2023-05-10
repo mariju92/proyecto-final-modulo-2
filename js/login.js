bf7f618a592c693e52b84048bfb2aaa59425b88c
Joaquin Fuentes <joaquin.fuentes1327@gmail.com>
2023-05-09 21:31:46 -0300

    coordine las funciones de login y carrito

--- js/login.js
+++ js/login.js
@@ -1,101 +1,143 @@
-const botonIniciarSesion = document.querySelector(".botonIniciarSesion");
-const mostrarModalInicioSesion = document.querySelector("#exampleModal");
-const FormularioLogin = document.querySelector(".formLogin");
-const Email = document.querySelector(".usuario");
-const password = document.querySelector(".password");
+import Usuario from "./classUsuario.js";
+
+const carritoSuperior = document.querySelector("#btnCarrito")
+const btnAdministrador = document.querySelector("#btnAdministrador")
+const alert = document.querySelector("#alert")
+const formLogin = document.querySelector("#formLogin")
+const email = document.querySelector("#email")
+const contrasenia = document.querySelector("#contrasenia")
+const botonIniciarSesion = document.querySelector("#btnIniciarSesion");
+const botonCerrarSesion = document.querySelector("#btnCerrarSesion");
+
+
+const modalLogin = new bootstrap.Modal(
+  document.querySelector("#modalLogin")
+);
+
+//traigo los usuarios de localstorage
+let listaUsuarios = localStorage.getItem("listaUsuarios");
+if (!listaUsuarios) {
+  listaUsuarios = []
+} else {
+  listaUsuarios = JSON.parse(listaUsuarios).map((usuario) =>
+    new Usuario(
+      usuario.id,
+      usuario.nombre,
+      usuario.email,
+      usuario.contrasenia,
+      usuario.rol,
+      usuario.carrito
+    )
+  )
+}
+let usuarioLogueado = JSON.parse(sessionStorage.getItem("usuarioLogueado"));
 
 //manejadores de eventos
-botonIniciarSesion.addEventListener("click", mostrarModalInicioSesion);
+botonIniciarSesion.addEventListener("click", mostrarModal);
+botonCerrarSesion.addEventListener("click", cerrarSesion);
+formLogin.addEventListener("submit", iniciarSesion)
 
-// cargar usuario de administrador
-const administrador = {
-  Email: "admin@gmail.com",
-  password: ".Code123",
-  rol: "administrador",
-};
+buscarRolDeUsuario()
+actualizarCarritoSuperior()
+//Buscar usuario en localstorage y mostrar lo que corresponda segun su rol
+function buscarRolDeUsuario(){
+  let usuarioLogueado = JSON.parse(sessionStorage.getItem("usuarioLogueado"));
+  if(usuarioLogueado){
+    if(usuarioLogueado.rol === "invitado"){
+      console.log("Usuario invitado")
+      btnAdministrador.className = "d-none"
+      botonIniciarSesion.className = "d-none"
+      botonCerrarSesion.className = "btn botonIniciarSesion"
+      carritoSuperior.className = "btn"
 
-verificarAdministrador();
-function verificarAdministrador() {
-  let esAdministrador = sessionStorage.getItem("administrador");
-  if (esAdministrador) {
-    botonIniciarSesion.innerHTML = "Cerrar Sesión";
-    
-  } else {
-    botonIniciarSesion.innerHTML = "Iniciar Sesión";
-    document.querySelector(".paginaAdministrador").classList.remove("d-none");
-    let webAdmin = window.location.origin + "/pages/administrador.html";
-    if (window.location.href === webAdmin) {
-      document.querySelector(".fondoNegro").innerHTML =
-        '<h2 class="text-center"> No posee los permisos para acceder a esta página, redireccionando a página principal</h2>';
-      setTimeout(() => {
-        window.location.href = window.location.origin;
-      }, 3000);
+
+    } else{
+      console.log("Usuario administrador")
+      btnAdministrador.className = "nav-link opcionNav paginaAdministrador"
+      botonIniciarSesion.className = "d-none"
+      botonCerrarSesion.className = "btn botonIniciarSesion"
+      carritoSuperior.className = "btn"
     }
+  } else{
+    console.log("No hay usuario logueado")
+    btnAdministrador.className = "d-none"
+    botonIniciarSesion.className = "btn botonIniciarSesion"
+    botonCerrarSesion.className = "d-none"
+    carritoSuperior.className = "d-none"
   }
+  
 }
+
 
 function mostrarModal() {
-  if (botonIniciarSesion.innerHTML === "IniciarSesion") {
-    mostrarModalInicioSesion.show();
-  } else {
-    CerrarSesion();
-  }
+  modalLogin.show();
 }
 
-function validarEmail() {
-  const regExp =
-    /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
-  if (regExp.test(Email.value)) {
-    Email.className = "form-control is-valid";
-    return true;
-  } else {
-    Email.className = "form-control is-invalid";
-    return false;
-  }
-}
+function iniciarSesion(e){
+  e.preventDefault()
+  //Validar campos
+  let usuarioEncontrado = listaUsuarios.find((usuario=>
+    ((usuario.email === email.value) && (usuario.contrasenia === contrasenia.value))))
+    if(usuarioEncontrado){
+      console.log(usuarioEncontrado)
+       //buscar rol de usuario
+       
+      //cargar en el sessionstorage
+      sessionStorage.setItem("usuarioLogueado", JSON.stringify(usuarioEncontrado));
 
-function validarPassword() {
-  const regExp =
-    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])([A-Za-z\d$@$!%*?&]|[^ ]){8,15}$/;
-  if (regExp.test(password.value)) {
-    password.className = "form-control is-valid";
-    return true;
-  } else {
-    password.className = "form-control is-invalid";
-    return false;
-  }
-}
+      //ocultar el modal
+      modalLogin.hide()
+      window.location.href = window.location.origin
 
-function IniciarSesion(e) {
-  e.preventDefault();
-  //validar input
-  if (validarEmail() && validarPassword()) {
-    if (
-      Email.value === administrador.Email &&
-      password.value === administrador.password
-    ) {
-      //guardo Administrador en LS y cambio el botón por cerrar sesión
-      alert.className = "alert alert-danger mt-3 d-none";
-      botonIniciarSesion.innerHTML = "Cerrar Sesión";
-      // Almaceno el objeto JSON en la sessionStorage
-      sessionStorage.setItem("administrador", JSON.stringify(administrador));
-      //habilito la opción del administrador en el navbar
-      document.querySelector(".paginaAdministrador").classList.remove("d-none");
-      mostrarModalInicioSesion.hide();
-    } else {
-      alert.className = "alert alert-danger mt-3";
+      } else{
+      console.log("ese usuario no esta registrado")
+        // si no se cumple la validacion
+        alert.className = "text-danger"
     }
-  }
+  
+        
+
+}
+function cerrarSesion(){
+  Swal.fire({
+    title: 'Estas seguro?',
+    text: "Seguro que deseas cerrar sesion?",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Aceptar',
+    cancelButtonText: "Cancelar"
+  }).then((result) => {
+    if (result.isConfirmed) {
+      sessionStorage.removeItem("usuarioLogueado")
+      buscarRolDeUsuario()
+      Swal.fire(
+        'Sesion CERRADA!',
+        'Tu sesion ha sido cerrada',
+        'success'
+      )
+       //redireccionar a la pagina principal
+       window.location.href = window.location.origin
+    }
+  })
+  
 }
 
-function CerrarSesion() {
-  sessionStorage.removeItem("administrador");
-  botonIniciarSesion.innerHTML = "Iniciar Sesión";
-  document.querySelector(".paginaAdministador").classList.add("d-none");
-  //Redireccionar a página de inicio
-  window.location.href = window.location.origin;
+
+
+
+//CODIGO QUE MUESTRA LA CANTIDAD DE PRODUCTOS EN EL CARRITO DEL NAVBAR
+function actualizarCarritoSuperior() {
+  let cantidadTotal = 0;
+  if(usuarioLogueado){
+    usuarioLogueado.carrito.map((carrito) => {
+      cantidadTotal = cantidadTotal + carrito.cantidad;
+      carritoSuperior.innerHTML = `<i
+    class="bi bi-cart-fill opcionNav carrito"></i><span
+    class="badge translate-middle bg-danger ">${cantidadTotal || 0}</span>`;
+  
+  })
 }
-
-
-
-
+}
+  
