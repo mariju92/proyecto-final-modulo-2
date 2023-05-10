09f92ae461d169ff5be5f139ed1f7cbd16aad086
Marianjuavi <mariana.ju92@gmail.com>
2023-05-09 23:23:39 -0300

    Se agregan eventos a los botones iniciar sesion e ingresar

--- js/login.js
+++ js/login.js
@@ -1,7 +1,12 @@
 //Declaro los constantes
-const btnIniciarSesion = document.querySelector("#iniciarSesion");
+const btnIniciarSesion = document.querySelector("#btnIniciarSesion");
 const btnIngresar = document.querySelector("#ingresar");
-//const pagAdmin = document.querySelector("#admin");
-const email = document.querySelector("#usuario");
+const pagAdmin = document.querySelector("#btnAdministrador");
+const email = document.querySelector("#email");
 const password = document.querySelector("#password");
 const modalLogin = new bootstrap.Modal (document.querySelector("#modalLogin"));
+
+//Agrego evento a boton ingresar
+btnIniciarSesion.addEventListener("click", mostrarModal);
+btnIngresar.addEventListener("submit", iniciarSesion);
+
